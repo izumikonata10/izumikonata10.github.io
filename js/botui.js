7f1359bb436cce5f21e50d9a8c4a8717972d95fb
izumikonata10 <2444639628@qq.com>
2026-09-22 23:17:51 +0800

    Site updated: 2026-09-22 23:17:50

--- js/botui.js
+++ js/botui.js
@@ -64,7 +64,7 @@ function bot_ui_ini() {
         end = function () {
             botui.message.add({
                 delay: 600,
-                content: "![...](https://view.moezx.cc/images/2018/05/06/a1c4cd0452528b572af37952489372b6.md.jpg)"
+                content: "![...](/images/site/content/botui.webp)"
             })
         },
         secondpart = function () {
